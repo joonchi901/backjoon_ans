52ad3c67f165f24718af2c4c8e01be33810eabbf
joonchi <jshan901@gmail.com>
2021-04-06 10:34:01 +0900

    code modified

--- string/2908.cpp
+++ string/2908.cpp
@@ -6,14 +6,17 @@ using namespace std;
 
 int main() {
 	string a, b;
-
+	int a_int, b_int;
 	cin >> a >> b;
 
 	reverse(a.begin(), a.end());
 	reverse(b.begin(), b.end());
 
-	if (a < b) cout << b;
-	else cout << a;
+	a_int = stoi(a);
+	b_int = stoi(b);
+
+	if (a_int < b_int) cout << b_int;
+	else cout << a_int;
 
 	return 0;
 }
